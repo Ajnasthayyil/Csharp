cb59744fb09a7c7205865789453ae5a0070fe7a9
AJNAS THAYYIL <105600255+Ajnasthayyil@users.noreply.github.com>
2025-11-27 16:50:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Accessing the inputs from the user 
+Console.Write("Enter Your First Number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Your Second Number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+//Arithematic Operations 
+Console.WriteLine( number+number1);
+Console.WriteLine( number-number1);
+Console.WriteLine( number*number1);
+Console.WriteLine(number/number1);
+
