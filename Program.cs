86298798d778a973f07f8086b602c0b7677c0e81
AJNAS THAYYIL <105600255+Ajnasthayyil@users.noreply.github.com>
2025-12-03 10:19:40 +0530

    Prime Number

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿//Accessing the inputs from the user 
-Console.Write("Enter Your First Number:");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Your Second Number:");
-int number1 = Convert.ToInt32(Console.ReadLine());
+﻿using System;
+class primeNUmber
+{
+    static void Main()
+    {
+        Console.Write("Enter Your Number:");
+        int num = int.Parse(Console.ReadLine());
 
-//Arithematic Operations 
-Console.WriteLine( number+number1);
-Console.WriteLine( number-number1);
-Console.WriteLine( number*number1);
-Console.WriteLine(number/number1);
+        bool isPrime = true;
 
+        if(num <= 1)
+        {
+            isPrime= false;
+        }
+
+        else
+        {
+            for(int i=2; i<=num/2; i++)
+            {
+                if(num % i == 0)
+                {
+                    isPrime= false;
+                    break;
+                }
+            }
+        }
+        if (isPrime)
+        {
+            Console.WriteLine($"{num} is a prime number");
+        }
+        else
+        {
+             Console.WriteLine($"{num} is not a prime number");
+
+        }
+    }
+}
